bba73480c8ade78acd87a6346fadbb560d2ebffa
Vinicio Brejinski <viniciosdb@gmail.com>
2025-02-19 18:01:01 -0300

    edit theme glue stack

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
-import { Text, View } from 'react-native'
-import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto'
+import { View, StatusBar } from 'react-native'
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto'
+import { GluestackUIProvider, Text, Center } from '@gluestack-ui/themed'
+import { config } from '@gluestack-ui/config'
 
 export function App() {
   const [fontsLoaded] = useFonts({
@@ -8,14 +14,15 @@ export function App() {
   })
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      {fontsLoaded ? <Text style={{ color: 'white' }}>Home</Text> : <View />}
-    </View>
+    <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparente"
+        translucent
+      />
+      <Center flex={1} bg="$info600">
+        {fontsLoaded ? <Text>Home</Text> : <View />}
+      </Center>
+    </GluestackUIProvider>
   )
 }
